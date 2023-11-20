a432a26b813925840065d6daca582ad8a0c342b8
Uddhav Jadhav <jadhavus.bmw@gmail.com>
2023-11-20 19:44:08 +0530

    api url env var import added to client

--- packages/client/src/utils/API.js
+++ packages/client/src/utils/API.js
@@ -1,5 +1,5 @@
 // local
-export const API = "http://localhost:8000/api/v1";
+export const API = import.meta.env.API_URL || "http://localhost:8000/api/v1";
 
 // build
 // export const API = "/api/v1"
