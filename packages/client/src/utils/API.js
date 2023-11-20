5e7d3e9cdb65b28d2fc4a7f94dfaff74c6b39695
Uddhav Jadhav <jadhavus.bmw@gmail.com>
2023-11-20 19:56:17 +0530

    vite env declaration bug solved

--- packages/client/src/utils/API.js
+++ packages/client/src/utils/API.js
@@ -1,5 +1,6 @@
 // local
-export const API = import.meta.env.API_URL || "http://localhost:8000/api/v1";
+export const API =
+  import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1";
 
 // build
 // export const API = "/api/v1"
